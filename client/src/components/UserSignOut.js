43c0f0b0cdd5a40595f7d03216c093227bb5dd9a
Le-Gomes <69648505+Le-Gomes@users.noreply.github.com>
2021-08-19 22:48:16 +0200

    Update UserSignOut.js
    Re-implementing this component with Hooks in order to avoid two errors.

--- client/src/components/UserSignOut.js
+++ client/src/components/UserSignOut.js
@@ -1,22 +1,30 @@
 // Importing React related modules
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 
 // Importing the Consumer object from the instance of the Context API
-import { Consumer } from '../Context';
+import Context from '../Context';
 
 const UserSignOut = () => {
-    return (
+    
+    /*  Using this Hook in order to avoid the error
+        'React Hook "useEffect" cannot be called inside a callback'
+        that happens when implementing the useEffect Hook inside
+        the callback function of:
         <Consumer>
         { context => {
-            context.actions.signOut();    // Call the signOut method from the Context API
+            useEffect( () =>  context.actions.signOut() );
+    */
+    const context = useContext(Context);
     
-            return (
-                <Redirect to="/" />     // And then redirects user to the default route
-            );
-        }}    
-        </Consumer>
-    )
+    /*  Using this Hook to avoid the error
+        'Cannot update during an existing state transition (such as within `render`)...'
+    */
+    useEffect( () => context.actions.signOut() );  // Call the signOut method from the Context API
+
+    return (
+        <Redirect to="/" />     // Redirects user to the default route
+    );
 }
 
 export default UserSignOut;
